3c437f50c4db30953531436a8be821e992c80439
aydincan77 <aydincan77@gmail.com>
2018-05-21 22:17:53 +0300

    first

--- Merhaba Dünya/ViewController.swift
+++ Merhaba Dünya/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlet tanımlandı
+    @IBOutlet weak var etiketLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Program yüklendiğinde Konsola aşağıdaki yazı yazılsın
+        print("Merhaba Dünya Uygulaması başlatıldı.")
+        
     }
 
+    // Buton'a action atandı
+    @IBAction func yazdırBtn(_ sender: Any) {
+        // etiketLbl'e yazı yazdırılsın
+        etiketLbl.text = "Merhaba"
+        
+        // butona basıldığında Konsola aşağıdaki yazı yazılsın
+        print("Butona basıldı ve label'e yazı yazıldı.")
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
